ccac44355a7fb2f93f4147b888c1312c0a9d4bae
eduardo <massami.uchida@gmail.com>
2019-03-05 23:27:07 -0300

    autenticação

--- src/main/routes.jsx
+++ src/main/routes.jsx
@@ -5,10 +5,11 @@ import { Router, Route, IndexRoute, Redirect, hashHistory } from 'react-router'
 import App from './app'
 import Dashboard2 from '../dashboard2/dashboard2'
 import BillingCycle from '../billingCycle/billingCycle'
+import AuthOrApp from './authOrApp'
 
 export default props => (
     <Router history={hashHistory}>
-        <Route path='/' component={App}>
+        <Route path='/' component={AuthOrApp}>
             <IndexRoute component={Dashboard2} />
             <Route path='billingCycles' component={BillingCycle} />
         </Route>
