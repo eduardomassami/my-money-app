6de4841e7011743f8dd3cd1dc1896f04a800d197
eduardo <massami.uchida@gmail.com>
2019-03-05 21:16:11 -0300

    faltando autenticação

--- src/common/template/header.jsx
+++ src/common/template/header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+// import Navbar from './navbar'
 
 export default props => (
     <header className='main-header'>
@@ -6,11 +7,11 @@ export default props => (
             <span className='logo-mini'><b> My</b>M</span>
             <span className='logo-lg'>
                 <i className='fa fa-money'></i>
-                <b> My</b>M
+                <b> My</b>Money
             </span>
         </a>
         <nav className='navbar navbar-static-top'>
-            <a href className='sidebar-toggle' data-toogle='offcanvas'></a>
+            <a href className='sidebar-toggle' data-toggle='offcanvas'></a>
         </nav>
     </header>
 )
